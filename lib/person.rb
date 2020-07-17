a4bb430f9dfbc2d7b4e4bbaacb797ec2f874f274
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-17 04:46:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -46,10 +46,10 @@ class Person
     self.happiness += 2
      p "♪ another one bites the dust ♫"
   end
-  def call_friend (Person)
+  def call_friend (friend)
     self.happiness += 3
-    Person.happiness += 3
-    p "Hi #{Person}! It's #{self}. How are you?"
+    friend.happiness += 3
+    p "Hi #{friend}! It's #{self}. How are you?"
   end
 end
   
