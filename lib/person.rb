08a6ba478eaa943ab9207cb1e77d19f739aef8b8
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-17 04:23:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,8 +8,8 @@ class Person
   def initialize (name, bank_account = 25, happiness = 8)
     @name = name
     @bank_account = bank_account
-    @happiness = happiness
     HAPPINESS = happiness
+    
   end
   
   
