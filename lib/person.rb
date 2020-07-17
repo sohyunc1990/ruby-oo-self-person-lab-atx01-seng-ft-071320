f7f138ece4b3c1542cb5f91aa6879b1218bad6b2
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-17 04:10:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,9 +4,10 @@ class Person
   attr_reader :name
   attr_accessor :bank_account
 
-  def initialize (name, bank_account = 25)
+  def initialize (name, bank_account = 25, happiness = 8)
     @name = name
     @bank_account = bank_account
+    @happiness = happiness
   end
   
   
