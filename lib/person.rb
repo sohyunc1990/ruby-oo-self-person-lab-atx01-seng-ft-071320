daa763d5396676a76f022726ea49bbfb786fb330
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-17 04:30:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -16,6 +16,11 @@ class Person
     @happiness = 0 if @happiness < 0
     @happiness
   end
+   def hygiene
+    @hygiene = 10 if @hygiene > 10
+    @happiness = 0 if @happiness < 0
+    @happiness
+  end
   
   
 
