e718da899f5932a1828b4ad2b99f34b7dae064a9
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-17 04:11:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 # your code goes here
 require'pry'
 class Person
-  attr_reader :name, :happiness
-  attr_accessor :bank_account
+  attr_reader :name
+  attr_accessor :bank_account, :happiness
 
-  def initialize (name, bank_account = 25, happiness = 9)
+  def initialize (name, bank_account = 25, happiness = 8)
     @name = name
     @bank_account = bank_account
     @happiness = happiness
