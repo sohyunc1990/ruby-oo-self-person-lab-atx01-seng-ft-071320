68f28edc4ba4803d3da014747f667f2185f3b442
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-17 04:25:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,12 +3,12 @@ require'pry'
 class Person
   attr_reader :name
   attr_accessor :bank_account, :happiness
-  HAPPINESS = (0..10)
+ 
 
   def initialize (name, bank_account = 25, happiness = 8)
     @name = name
     @bank_account = bank_account
-    HAPPINESS = happiness
+    @happiness(0..10) = happiness
     
   end
   
